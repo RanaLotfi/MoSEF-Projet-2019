bf7022a927f3973f4884f41b2a56a54e8359377f
Sorbonne <rana_lotfi_1998@hotmail.com>
2019-11-17 16:48:46 +0100

    ajout de grep a find

--- search_fichiers.sh
+++ search_fichiers.sh
@@ -4,4 +4,6 @@ read -p "Quel repertoire vous interesse aujourd'hui? " repertoire
 cd /home/$USERNAME/$repertoire
 
 echo "Nous cherchons le(s) fichier(s) compatible(s) avec le nom que vous aviez mis en argument" 
-find -name "$@" || less
+echo `find -name "$1" -exec grep -Hi "$2" {} \;`
+
+
